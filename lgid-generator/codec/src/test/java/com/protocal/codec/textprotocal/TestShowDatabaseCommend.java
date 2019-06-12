05b095d4110014fd1b758ddd5848d3aa465ce92f
dengcheng <dengcheng@lvmama.com>
2019-06-12 09:04:08 +0800

    add show database com packet

---
+++ lgid-generator/codec/src/test/java/com/protocal/codec/textprotocal/TestShowDatabaseCommend.java
@@ -0,0 +1,33 @@
+package com.protocal.codec.textprotocal;
+
+import com.lvmama.infrastructure.codec.utils.PacketBuilder;
+import com.lvmama.infrastructure.protocal.message.response.PacketWrapper;
+import com.lvmama.infrastructure.protocal.message.response.client.MysqlTextPacket;
+import com.protocal.codec.handshake.BaseTest;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import org.junit.Test;
+
+/**
+ * @Auther: dengcheng
+ * @Date: 2019/6/12 09:00
+ * @Description:
+ */
+public class TestShowDatabaseCommend extends BaseTest {
+
+    @Test
+    public void testRep(){
+        ByteBuf byteBuf = Unpooled.buffer();
+        byteBuf.writeBytes(this.StringToBytes("textprotocal/rep/show_database_rep.txt"));
+        System.out.println(ByteBufUtil.prettyHexDump(byteBuf));
+    }
+
+    @Test
+    public void testReq(){
+        ByteBuf byteBuf = Unpooled.buffer();
+        byteBuf.writeBytes(this.StringToBytes("textprotocal/show_database_req.txt"));
+        PacketWrapper<MysqlTextPacket> packets = PacketBuilder.getInstance().buildMysqlTextProtocalPackt(byteBuf);
+        System.out.println(packets.getPackets().toString());
+    }
+}
